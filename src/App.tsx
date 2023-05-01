755c5fa333ee578e2ce26fc3dacf08b23ae405d8
Alex Mata <alexmatasoftware@gmail.com>
2023-05-01 10:14:58 -0500

    WIP need to add esbuild to public when cloned

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
+import * as esbuild from 'esbuild-wasm'
+import { useEffect, useRef, useState } from 'react'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+
 const App = () => {
+  const ref = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  // init esbuild service
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const handleSubmit = async () => {
+    if (!ref.current) return
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    })
+
+    setCode(result.outputFiles[0].text)
+  }
+
   return (
     <main>
       <h1>DocBook</h1>
+      <div className='codeEditorBlock'>
+        <textarea
+          className='codeEditorBlock__editor'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <div className='codeEditorBlock__buttons'>
+          <button
+            className='codeEditorBlock__button codeEditorBlock__button--submit'
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+        <div className='codeEditorBlock__preview'>
+          <pre>{code}</pre>
+        </div>
+      </div>
     </main>
   )
 }
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -35,8 +35,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'nested-test-pkg';
-              console.log(message);
+              import react from 'react';
+              console.log(react);
             `
           }
         }
