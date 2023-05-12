f85b767b0d1e1e3fb130aaf63000dac05f28bce5
denimcouch <alexmata92@ymail.com>
2023-05-11 19:25:50 -0500

    🐛 bugfix addressing resize breaking at smaller viewports

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -10,6 +10,7 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ resizeDirection, children }) => {
   const [innerHeight, setInnerHeight] = useState(window.innerHeight)
   const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(innerWidth * 0.75)
 
   useEffect(() => {
     let timer: ReturnType<typeof setTimeout>
@@ -21,6 +22,9 @@ const Resizable: React.FC<ResizableProps> = ({ resizeDirection, children }) => {
       timer = setTimeout(() => {
         setInnerHeight(window.innerHeight)
         setInnerWidth(window.innerWidth)
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75)
+        }
       }, 100)
     }
 
@@ -29,7 +33,7 @@ const Resizable: React.FC<ResizableProps> = ({ resizeDirection, children }) => {
     return () => {
       window.removeEventListener('resize', listener)
     }
-  }, [])
+  }, [width])
 
   const resizableProps: ResizableBoxProps =
     resizeDirection === 'vertical'
@@ -42,11 +46,14 @@ const Resizable: React.FC<ResizableProps> = ({ resizeDirection, children }) => {
         }
       : {
           height: Infinity,
-          width: innerWidth * 0.75,
+          width,
           resizeHandles: ['e'],
           minConstraints: [innerWidth * 0.35, Infinity],
           maxConstraints: [innerWidth * 0.75, Infinity],
-          className: 'resize-horizontal'
+          className: 'resize-horizontal',
+          onResizeStop(e, data) {
+            setWidth(data.size.width)
+          }
         }
 
   return <ResizableBox {...resizableProps}>{children}</ResizableBox>
